b3a9027a2203327399b9b8301f971d271a737f93
Marcelo Perez Alvarado <chelo1150@gmail.com>
2019-11-23 14:41:17 -0600

    Commit All

---
+++ MVC_Homework/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿namespace MVC_Homework.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Models;
+    public class HomeController : Controller
+    {
+        private int actual = 0;
+        private int pass = 0;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ThreeNumbers model)
+        {
+            string text;
+            pass = actual;
+            actual = model.RandomNumber;
+            
+            text = actual > pass ? "mayor" : "menor";
+            ViewBag.Text = "El número actual es " + text;
+            ViewBag.Last = pass;
+            ViewBag.Random = model.RandomNumber;
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
---
+++ MVC_Homework/Models/ThreeNumbers.cs
@@ -0,0 +1,19 @@
+﻿namespace MVC_Homework.Models
+{
+    using System;
+
+    public class ThreeNumbers
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int RandomNumber
+        {
+            get
+            {
+                Random r = new Random();
+                return r.Next(FirstNumber, SecondNumber);
+            }
+        }
+    }
+}
